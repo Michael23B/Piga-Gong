e6372f46ac4cf5cb61d7fa5a4d5795bdeeae8f68
Jon Alfaro <j_alfa_4@outlook.com>
2020-04-22 00:33:50 +1000

    Gong scene updated to have sample player with new camera Controls. Adiitionally CameraFollow.script can now translate mouse movement to pitch and yaw around object.

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject playerObject;
+
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+
+    public float mouseSensitivity = 0.5f;
+
+    void FixedUpdate()
+    {
+        var playerTransform = playerObject.transform;
+        var controllerTransform = transform;
+        var controllerRotation = controllerTransform.rotation;
+
+        // Follow Target
+        Vector3 desiredPosition = playerTransform.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(controllerTransform.position, desiredPosition, smoothSpeed);
+
+        transform.position = smoothedPosition;
+
+        // Mouse Rotation Handler
+        // controllerTransform.Rotate( Input.GetAxis("Mouse Y"),Input.GetAxis("Mouse X"),0f);
+
+        var yaw = Quaternion.Euler(0f, Input.GetAxis("Mouse X") * mouseSensitivity, 0f);
+        var pitch = Quaternion.Euler(-Input.GetAxis("Mouse Y") * mouseSensitivity, 0f, 0f);
+        transform.rotation =  yaw * controllerRotation * pitch; // yaw on the left && pitch on the right?
+        
+        // Copy Rotation
+        playerTransform.transform.rotation = controllerRotation;
+        
+        
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody rb;
+    public Camera cm1;
+    public int speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var position = transform.position;
+        var xVelMod = 0;
+        var yVelMod = 0;
+        var zVelMod = 0;
+        var VectorMod = new Vector3(0,0,0);
+        
+        if (Input.GetKey(KeyCode.W))
+        {
+            VectorMod += transform.forward;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            VectorMod -= transform.forward;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            VectorMod -= transform.right;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            VectorMod += transform.right;
+        }
+
+        rb.velocity += VectorMod*speed;
+    }
+}
