d8ffd45939fec35cc6163b9966e978a16dcbd553
Michael Burnett <michael@inthecode.com.au>
2020-04-25 11:48:22 +1000

    Added player score UI
    Can be updated and reset with events.

---
+++ Assets/Scripts/PlayerScoreUI.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScoreUI : MonoBehaviour
+{
+    public string scoreText = "Boints: {score}";
+    public Text text;
+    private float playerScore = 0;
+    private string displayedText;
+
+    public void IncreaseScore()
+    {
+        playerScore++;
+        RedrawText();
+    }
+
+    public void ResetScore()
+    {
+        playerScore = 0;
+        RedrawText();
+    }
+
+    private void RedrawText()
+    {
+        text.text = scoreText.Replace("{score}", playerScore.ToString());
+    }
+}
